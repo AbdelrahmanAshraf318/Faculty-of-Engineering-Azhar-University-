60d78cf509ce57f1921db9c1c2bf26356a4c61c2
Abdelrahman Ashraf <abdelrahmanashraf25102000@gmail.comm>
2021-09-12 15:08:13 +0200

    Dean Word file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import SideShow from './components/SideShow/SideShow';
 import './App.css'
+import DeanWord from "./components/DeanWord/DeanWord";
 
 class App extends Component
 {
@@ -17,6 +18,15 @@ class App extends Component
         </header>
 
         <SideShow />
+
+        <div className="parent">
+            <div className="child-2">
+
+            </div>
+            <div className="child-1">
+              <DeanWord  />
+            </div>
+        </div>
         
         <footer>
           <Footer/>
--- src/components/DeanWord/DeanWord.js
+++ src/components/DeanWord/DeanWord.js
@@ -1,13 +1,22 @@
 import React , {Component}  from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './DeanWord.css';
+import dean from '../images/dean.jpg';
 
 class DeanWord extends Component
 {
     render(){
         return(
-           <div className="parent">
-               <h1>كلمة العمداء والوكلاء</h1>
-           </div> 
+            <div className="dean">
+                <div className="theImg">
+                        <img src={dean} alt="Snow" />
+                        <div class="bottom-left">كلمة الأستاذ عميد الكليه</div>
+                </div>
+                <div className="box">
+                    <img src={dean} alt="Snow" />
+                    <p>كلمة الأستاذ عميد الكليه</p>
+                </div>
+            </div>
         )
     }
 }
